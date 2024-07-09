b5a9c9d3295153fb5a84df75974480851c19c94c
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-09 18:00:35 +0530

    Tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,26 +1,15 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.apperiance}>
-      <Text style={styles.text}>App</Text>
+    <SafeAreaView 
+    className='bg-green-200 items-center'
+    
+    >
+      <Text className="text-2xl text-red-600 dark:text-white font-extrabold ">App</Text>
     </SafeAreaView>
   )
 }
 
-export default App
-
-const styles = StyleSheet.create({
-  apperiance:{
-    flex:1,
-    alignItems:'center',
-   justifyContent:'center',
-  },
-  text:{
-    color:'red',
-    fontSize:20,
-    fontWeight:'bold',
-    
-  }
-})
+export default App;
